757248b74568c038ff0d09f6d3b52980206f5b52
Nawal Jaiswal <naval1997@gmail.com>
2025-03-16 01:34:23 -0300

    initial nightwatch.js project stup

---
+++ test/duckDuckGo.js
@@ -0,0 +1,12 @@
+describe('duckduckgo example', function() {
+  this.tags = ['smoke'];
+  it('Search Nightwatch.js and check results', function(browser) {
+    browser
+      .navigateTo('https://duckduckgo.com')
+      .waitForElementVisible('input[name=q]')
+      .sendKeys('input[name=q]', ['Nightwatch.js'])
+      .click('*[type="submit"]')
+      .assert.visible('#react-layout')
+      .assert.textContains('#react-layout', 'Nightwatch.js');
+  }); 
+});
