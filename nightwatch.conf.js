d7a0557d28ee8df3c5e1f6582a70fbdfcb5a6c74
Nawal Jaiswal <naval1997@gmail.com>
2025-03-16 02:06:18 -0300

    Allure and workflow file

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,16 +1,3 @@
-// Refer to the online docs for more details:
-// https://nightwatchjs.org/gettingstarted/configuration/
-//
-
-//  _   _  _         _      _                     _          _
-// | \ | |(_)       | |    | |                   | |        | |
-// |  \| | _   __ _ | |__  | |_ __      __  __ _ | |_   ___ | |__
-// | . ` || | / _` || '_ \ | __|\ \ /\ / / / _` || __| / __|| '_ \
-// | |\  || || (_| || | | || |_  \ V  V / | (_| || |_ | (__ | | | |
-// \_| \_/|_| \__, ||_| |_| \__|  \_/\_/   \__,_| \__| \___||_| |_|
-//             __/ |
-//            |___/
-
 module.exports = {
   // An array of folders (excluding subfolders) where your tests are located;
   // if this is not specified, the test source must be passed as the second argument to the test runner.
@@ -41,6 +28,12 @@ module.exports = {
     default: {
       disable_error_log: false,
       launch_url: 'http://localhost',
+
+      globals: {
+        reporter: (results, done) => {
+          require('nightwatch-allure').write(results, done);
+        }
+      },
 
       screenshots: {
         enabled: false,
